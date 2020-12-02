bb6b28fefabcb507a3a046d2db2ec1075ab2422c
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-12-02 14:14:17 +0300

    handlers + state mashine

---
+++ handlers/users/start.py
@@ -0,0 +1,29 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.utils.deep_linking import get_start_link
+from filters import IsPrivate
+from loader import dp
+from re import compile
+
+
+@dp.message_handler(CommandStart(deep_link=compile(r'\d\d\d')),IsPrivate())
+async def bot_start(message: types.Message):
+    deep_link_args = message.get_args()
+
+    await message.answer(f'Привет, {message.from_user.full_name}!\n'
+                         f'Вы находитесь в личной переписке. \n'
+                         f'В вашей команде есть диплинк\n'
+                         f'Вы передали аргумент {deep_link_args}.\n')
+
+
+@dp.message_handler(CommandStart(deep_link=None), IsPrivate())
+async def bot_start(message: types.Message):
+    bot_user = await dp.bot.get_me()
+
+    # deep_link = f"http://t.me/{bot_user.username}?start=123"
+    deep_link = await get_start_link('123')
+
+    await message.answer(f'Привет, {message.from_user.full_name}!\n'
+                         f'Вы находитесь в личной переписке. \n'
+                         f'В вашей команде нет диплинка.\n'
+                         f'Ваша диплинк ссылка - {deep_link}')
